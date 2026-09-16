9bd7f8237b8d1c1f38b99964acc611c0a157447e
Yusuke Kawashita <kawasan.design@xs418243.xsrv.jp>
2026-09-16 10:42:37 +0900

    ユーザー登録機能を作成

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "ユーザー登録画面を表示できる" do
+    get new_user_path
+
+    assert_response :success
+  end
+
+  test "正しい入力でユーザーを登録できる" do
+    assert_difference("User.count", 1) do
+      post users_path, params: {
+        user: {
+          name: "登録テスト",
+          email: "new-user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+
+    assert_redirected_to root_path
+  end
+
+  test "不正な入力ではユーザーを登録できない" do
+    assert_no_difference("User.count") do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "",
+          password: "",
+          password_confirmation: ""
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+  end
+end
