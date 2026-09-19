4968b460f31dbfd1e9f429fcb80d2155cdef670b
Yusuke Kawashita <kawasan.design@xs418243.xsrv.jp>
2026-09-19 17:41:11 +0900

    共通ヘッダーとナビゲーションを作成

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -7,14 +7,17 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "未ログイン時はログアウトボタンが表示されない" do
+  test "未ログイン時はユーザー登録とログインが表示される" do
     get root_url
 
     assert_response :success
+    assert_match "Norutoki", response.body
+    assert_match "ユーザー登録", response.body
+    assert_match "ログイン", response.body
     assert_no_match "ログアウト", response.body
   end
 
-  test "ログイン時はログアウトボタンが表示される" do
+  test "ログイン時はユーザー名とログアウトが表示される" do
     user = users(:one)
 
     post session_path, params: {
@@ -25,6 +28,8 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
 
     assert_response :success
+    assert_match user.name, response.body
     assert_match "ログアウト", response.body
+    assert_no_match "ユーザー登録", response.body
   end
 end
