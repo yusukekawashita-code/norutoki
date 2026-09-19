2dad53c77750a27eab1c47e12bbbb9c6585ab688
Yusuke Kawashita <kawasan.design@xs418243.xsrv.jp>
2026-09-19 16:46:19 +0900

    ログアウト機能の動作確認を追加

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,28 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
+
     assert_response :success
+  end
+
+  test "未ログイン時はログアウトボタンが表示されない" do
+    get root_url
+
+    assert_response :success
+    assert_no_match "ログアウト", response.body
+  end
+
+  test "ログイン時はログアウトボタンが表示される" do
+    user = users(:one)
+
+    post session_path, params: {
+      email: user.email,
+      password: "password"
+    }
+
+    get root_url
+
+    assert_response :success
+    assert_match "ログアウト", response.body
   end
 end
