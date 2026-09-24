a0868512f674fb556df6ac50a572f7c78c34266d
Yusuke Kawashita <kawasan.design@xs418243.xsrv.jp>
2026-09-24 23:06:22 +0900

    時刻表一覧・編集機能を作成

--- test/controllers/timetables_controller_test.rb
+++ test/controllers/timetables_controller_test.rb
@@ -108,4 +108,158 @@ class TimetablesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to new_session_path
     assert_equal "ログインしてください", flash[:alert]
   end
+
+    test "ログイン中のユーザーは自分のルートの時刻表一覧を表示できる" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    get usual_route_timetables_path(@usual_route)
+
+    assert_response :success
+    assert_select "h1", "時刻表一覧"
+    assert_select ".timetable-index__card"
+  end
+
+  test "時刻表一覧では出発時刻が早い順に表示される" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    timetable = timetables(:one)
+
+    timetable.departures.create!(
+      departure_time: "10:00",
+      note: "遅い便"
+    )
+
+    timetable.departures.create!(
+      departure_time: "07:30",
+      note: "早い便"
+    )
+
+    get usual_route_timetables_path(@usual_route)
+
+    assert_response :success
+
+    times = css_select(".timetable-index__time").map { |element| element.text.strip }
+
+    assert_equal times.sort, times
+    assert_includes times, "07:30"
+    assert_includes times, "10:00"
+  end
+
+  test "ログイン中のユーザーは自分の時刻表編集画面を表示できる" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    timetable = timetables(:one)
+
+    get edit_usual_route_timetable_path(@usual_route, timetable)
+
+    assert_response :success
+    assert_select "h1", "時刻表を編集"
+  end
+
+  test "ログイン中のユーザーは自分の時刻表を更新できる" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    timetable = timetables(:one)
+    departure = departures(:one)
+
+    patch usual_route_timetable_path(@usual_route, timetable), params: {
+      timetable: {
+        day_type: 1,
+        departures_attributes: {
+          "0" => {
+            id: departure.id,
+            departure_time: "08:20",
+            note: "更新後"
+          }
+        }
+      }
+    }
+
+    timetable.reload
+    departure.reload
+
+    assert_equal 1, timetable.day_type
+    assert_equal "08:20", departure.departure_time.strftime("%H:%M")
+    assert_equal "更新後", departure.note
+    assert_redirected_to usual_route_timetables_path(@usual_route)
+    assert_equal "時刻表を更新しました", flash[:notice]
+  end
+
+  test "不正な値では時刻表を更新できない" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    timetable = timetables(:one)
+    departure = departures(:one)
+    original_time = departure.departure_time
+
+    patch usual_route_timetable_path(@usual_route, timetable), params: {
+      timetable: {
+        day_type: timetable.day_type,
+        departures_attributes: {
+          "0" => {
+            id: departure.id,
+            departure_time: "",
+            note: departure.note
+          }
+        }
+      }
+    }
+
+    assert_response :unprocessable_entity
+    assert_select ".timetable-form__errors"
+
+    departure.reload
+    assert_equal original_time, departure.departure_time
+  end
+
+  test "他のユーザーの時刻表編集画面は表示できない" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    other_route = usual_routes(:two)
+    other_timetable = timetables(:two)
+
+    get edit_usual_route_timetable_path(other_route, other_timetable)
+
+    assert_response :not_found
+  end
+
+  test "他のユーザーの時刻表は更新できない" do
+    post session_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+    other_route = usual_routes(:two)
+    other_timetable = timetables(:two)
+    original_day_type = other_timetable.day_type
+
+    patch usual_route_timetable_path(other_route, other_timetable), params: {
+      timetable: {
+        day_type: 0
+      }
+    }
+
+    assert_response :not_found
+
+    other_timetable.reload
+    assert_equal original_day_type, other_timetable.day_type
+  end
 end
